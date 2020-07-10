f4bcb87531ff58e7dab149a2547f432680362ced
Rahulbanjare95 <rahul.banjare95@gmail.com>
2020-07-10 20:31:32 +0530

    Added: Test to check if less distance given then return minimum fare

--- src/test/java/InvoiceGeneratorTest.java
+++ src/test/java/InvoiceGeneratorTest.java
@@ -11,4 +11,13 @@ public class InvoiceGeneratorTest{
         double fare = invoiceGenerator.calculateFare(distance, time);
         Assert.assertEquals(25,fare,0.0);
     }
+
+    @Test
+    public void givenLessDistance_ShouldReturnMinimumFare() {
+        InvoiceGenerator invoiceGenerator = new InvoiceGenerator();
+        double distance = 0.1;
+        double time = 1;
+        double fare = invoiceGenerator.calculateFare(distance, time);
+        Assert.assertEquals(5,fare,0.0);
+    }
 }
