2a1ad5fbe37868841a53fd853cb9efd2a6e51d6d
Rahulbanjare95 <rahul.banjare95@gmail.com>
2020-07-11 09:41:39 +0530

    Refactored: Test cases for invoice summary when user id is given

--- src/test/java/InvoiceGeneratorTest.java
+++ src/test/java/InvoiceGeneratorTest.java
@@ -8,7 +8,7 @@ public class InvoiceGeneratorTest{
         InvoiceGenerator invoiceGenerator = new InvoiceGenerator();
          double distance  = 2.0;
          int time = 5;
-        double fare = invoiceGenerator.calculateFare(distance, time);
+        double fare = invoiceGenerator.calculateFare(distance, time,SubscriptionType.NORMAL);
         Assert.assertEquals(25,fare,0.0);
     }
 
@@ -17,19 +17,37 @@ public class InvoiceGeneratorTest{
         InvoiceGenerator invoiceGenerator = new InvoiceGenerator();
         double distance = 0.1;
         double time = 1;
-        double fare = invoiceGenerator.calculateFare(distance, time);
+        double fare = invoiceGenerator.calculateFare(distance, time,SubscriptionType.NORMAL);
         Assert.assertEquals(5,fare,0.0);
     }
 
     @Test
     public void givenMultipleRides_ShouldReturnTotalFare() {
         InvoiceGenerator invoiceGenerator = new InvoiceGenerator();
-        Ride[] rides = { new Ride(12,1),
-                         new Ride(25,2),
-                         new Ride(150,4),
-                         new Ride(15,1)};
-        InvoiceSummary invoiceSummary = invoiceGenerator.calculateFare(rides);
+        Ride[] rides = { new Ride(12,1,SubscriptionType.NORMAL),
+                         new Ride(25,2,SubscriptionType.NORMAL),
+                         new Ride(150,4,SubscriptionType.NORMAL),
+                         new Ride(15,1,SubscriptionType.NORMAL)};
+        String userId="asd@com";
+        invoiceGenerator.addRide(userId, rides);
+        InvoiceSummary invoiceSummary = invoiceGenerator.getInvoiceSummary(userId);
         InvoiceSummary expected = new InvoiceSummary(4,2028);
         Assert.assertEquals(expected,invoiceSummary);
+    }
+
+    @Test
+    public void givenUserIdAndRides_ShouldReturnInvoiceSummary() {
+        try {
+            InvoiceGenerator  invoiceGenerator =  new InvoiceGenerator();
+            String userId= "rahul@gmail.com";
+            Ride[] rides = {new Ride(2,5,SubscriptionType.NORMAL),new Ride(4,6,SubscriptionType.NORMAL)};
+            invoiceGenerator.addRide
+                    (userId,rides);
+            InvoiceSummary  summary =invoiceGenerator.getInvoiceSummary(userId);
+            InvoiceSummary expectedSummary = new InvoiceSummary(2,71.0);
+            Assert.assertEquals(expectedSummary.totalFare,summary.totalFare,0.0);
+        }catch (NullPointerException e){
+            e.printStackTrace();
+        }
     }
 }
