8b304af6e25e50d848c0c0e249e418fd606fdcb0
Rahulbanjare95 <rahul.banjare95@gmail.com>
2020-07-11 09:43:33 +0530

    Added: Code to define Ride and initialise time distance SubscriptionType

--- src/main/java/Ride.java
+++ src/main/java/Ride.java
@@ -1,10 +1,13 @@
 public class Ride {
     public double distance;
      public double time;
+    public  SubscriptionType type;
 
-    public Ride(double distance, double time) {
+
+    public Ride(double distance, double time, SubscriptionType type) {
         this.distance = distance;
         this.time = time;
+        this.type = type;
     }
 
 }
