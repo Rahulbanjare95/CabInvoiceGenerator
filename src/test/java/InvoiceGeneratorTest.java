d7c0fddf09658b90801af74047f402e0b6ce40ac
Rahulbanjare95 <rahul.banjare95@gmail.com>
2020-07-11 09:49:46 +0530

    Added: Test case to check for premimum rides

--- src/test/java/InvoiceGeneratorTest.java
+++ src/test/java/InvoiceGeneratorTest.java
@@ -1,15 +1,15 @@
 import org.junit.Assert;
 import org.junit.Test;
 
-public class InvoiceGeneratorTest{
+public class InvoiceGeneratorTest {
 
     @Test
     public void givenDistanceAndTime_ShouldReturnTotalFare() {
         InvoiceGenerator invoiceGenerator = new InvoiceGenerator();
-         double distance  = 2.0;
-         int time = 5;
-        double fare = invoiceGenerator.calculateFare(distance, time,SubscriptionType.NORMAL);
-        Assert.assertEquals(25,fare,0.0);
+        double distance = 2.0;
+        int time = 5;
+        double fare = invoiceGenerator.calculateFare(distance, time, SubscriptionType.NORMAL);
+        Assert.assertEquals(25, fare, 0.0);
     }
 
     @Test
@@ -17,36 +17,51 @@ public class InvoiceGeneratorTest{
         InvoiceGenerator invoiceGenerator = new InvoiceGenerator();
         double distance = 0.1;
         double time = 1;
-        double fare = invoiceGenerator.calculateFare(distance, time,SubscriptionType.NORMAL);
-        Assert.assertEquals(5,fare,0.0);
+        double fare = invoiceGenerator.calculateFare(distance, time, SubscriptionType.NORMAL);
+        Assert.assertEquals(5, fare, 0.0);
     }
 
     @Test
     public void givenMultipleRides_ShouldReturnTotalFare() {
         InvoiceGenerator invoiceGenerator = new InvoiceGenerator();
-        Ride[] rides = { new Ride(12,1,SubscriptionType.NORMAL),
-                         new Ride(25,2,SubscriptionType.NORMAL),
-                         new Ride(150,4,SubscriptionType.NORMAL),
-                         new Ride(15,1,SubscriptionType.NORMAL)};
-        String userId="asd@com";
+        Ride[] rides = {new Ride(12, 1, SubscriptionType.NORMAL),
+                new Ride(25, 2, SubscriptionType.NORMAL),
+                new Ride(150, 4, SubscriptionType.NORMAL),
+                new Ride(15, 1, SubscriptionType.NORMAL)};
+        String userId = "asd@com";
         invoiceGenerator.addRide(userId, rides);
         InvoiceSummary invoiceSummary = invoiceGenerator.getInvoiceSummary(userId);
-        InvoiceSummary expected = new InvoiceSummary(4,2028);
-        Assert.assertEquals(expected,invoiceSummary);
+        InvoiceSummary expected = new InvoiceSummary(4, 2028);
+        Assert.assertEquals(expected, invoiceSummary);
     }
 
     @Test
     public void givenUserIdAndRides_ShouldReturnInvoiceSummary() {
         try {
-            InvoiceGenerator  invoiceGenerator =  new InvoiceGenerator();
-            String userId= "rahul@gmail.com";
-            Ride[] rides = {new Ride(2,5,SubscriptionType.NORMAL),new Ride(4,6,SubscriptionType.NORMAL)};
+            InvoiceGenerator invoiceGenerator = new InvoiceGenerator();
+            String userId = "rahul@gmail.com";
+            Ride[] rides = {new Ride(2, 5, SubscriptionType.NORMAL), new Ride(4, 6, SubscriptionType.NORMAL)};
             invoiceGenerator.addRide
-                    (userId,rides);
-            InvoiceSummary  summary =invoiceGenerator.getInvoiceSummary(userId);
-            InvoiceSummary expectedSummary = new InvoiceSummary(2,71.0);
-            Assert.assertEquals(expectedSummary.totalFare,summary.totalFare,0.0);
-        }catch (NullPointerException e){
+                    (userId, rides);
+            InvoiceSummary summary = invoiceGenerator.getInvoiceSummary(userId);
+            InvoiceSummary expectedSummary = new InvoiceSummary(2, 71.0);
+            Assert.assertEquals(expectedSummary.totalFare, summary.totalFare, 0.0);
+        } catch (NullPointerException e) {
+            e.printStackTrace();
+        }
+    }
+    @Test
+    public void givenUserIdAndPremiumRides_ShouldReturnInvoiceSummary() {
+        try {
+            InvoiceGenerator invoiceGenerator = new InvoiceGenerator();
+            String userId = "rahul@gmail.com";
+            Ride[] rides = {new Ride(10, 2, SubscriptionType.PREMIUM), new Ride(14, 6, SubscriptionType.PREMIUM)};
+            invoiceGenerator.addRide
+                    (userId, rides);
+            InvoiceSummary summary = invoiceGenerator.getInvoiceSummary(userId);
+            InvoiceSummary expectedSummary = new InvoiceSummary(2, 248.0);
+            Assert.assertEquals(expectedSummary.totalFare, summary.totalFare, 0.0);
+        } catch (NullPointerException e) {
             e.printStackTrace();
         }
     }
