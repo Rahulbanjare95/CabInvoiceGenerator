6cf144f02f76c992ad6e9c4f6b105faa6a196e2c
Rahulbanjare95 <rahul.banjare95@gmail.com>
2020-07-11 09:42:23 +0530

    Refactored: Code to calculate invoice summary based on user id

--- src/main/java/InvoiceGenerator.java
+++ src/main/java/InvoiceGenerator.java
@@ -1,22 +1,41 @@
+import java.util.ArrayList;
+
 public class InvoiceGenerator {
 
+    RideRepository  rideRepository = null;
+    InvoiceGenerator(){
+        this.rideRepository = new RideRepository();
+    }
+    private static double COST_PER_MINUTE = 1.0 ;
+    private static double MINIMUM_FARE = 5.0;
+    public double COST_PER_KM =10.0;
 
-    private static final double COST_PER_MINUTE = 1.0 ;
-    private static final double MINIMUM_FARE = 5.0;
-    public final double COST_PER_KM =10.0;
-
-    public double calculateFare(double distance, double time) {
+    public double calculateFare(double distance, double time, SubscriptionType type) {
+        setValue(type);
         double totalFare = (distance*COST_PER_KM)+(time*COST_PER_MINUTE);
         if (totalFare<MINIMUM_FARE)
             return MINIMUM_FARE;
     return totalFare;
     }
+    private void setValue(SubscriptionType type) {
+    this.COST_PER_KM=COST_PER_KM;
+    this.COST_PER_MINUTE =COST_PER_MINUTE;
+    this.MINIMUM_FARE =MINIMUM_FARE;
 
-    public InvoiceSummary calculateFare(Ride[] rides) {
+    }
+    public InvoiceSummary getInvoiceSummary(String userId) {
         double totalFare = 0.0;
+        ArrayList userRides = rideRepository.getRideDetails(userId);
+        Ride[] rides=  new Ride[userRides.size()];
+        userRides.toArray(rides);
         for (Ride ride:rides) {
-            totalFare += this.calculateFare(ride.distance, ride.time);
+            totalFare += this.calculateFare(ride.distance, ride.time , ride.type);
+
         }
         return new InvoiceSummary(rides.length,totalFare);
+    }
+
+    public void addRide(String userId, Ride[] rides) {
+        rideRepository.addRides(userId, rides);
     }
 }
